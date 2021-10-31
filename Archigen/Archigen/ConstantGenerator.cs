8fe9ea005bd327745758966d8d99cd7b187a19bd
kesac <kesac@users.noreply.github.com>
2021-10-31 02:11:02 -0600

    Added ConstantGenerator<T> + This is a convenience class to be used by Generator for properties that need to be the same value across the entire output space.

---
+++ Archigen/Archigen/ConstantGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archigen
+{
+    /// <summary>
+    /// This is a convience class to be used <see cref="Generator{T}"/>
+    /// when you are generating a complex object and a specific property
+    /// needs to be the same value across the entire output space.
+    /// </summary>
+    public class ConstantGenerator<T> : IGenerator<T>
+    {
+        private T Value;
+
+        public ConstantGenerator(T value) => this.Value = value;
+
+        public T Next() => this.Value;
+    }
+}
