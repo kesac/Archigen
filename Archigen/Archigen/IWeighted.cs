e914b83a137833b6d4515a91c0051be240191955
kesac <kevin@turtlesort.com>
2024-11-14 21:10:44 -0700

    Added a basic selector that works with weighted values

---
+++ Archigen/Archigen/IWeighted.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archigen
+{
+    /// <summary>
+    /// <para>
+    /// Represents an element that can
+    /// be randomly selected from a list and has 
+    /// a weight value that affects how frequently it
+    /// should be selected compared to others.
+    /// </para>
+    /// </summary>
+    public interface IWeighted
+    {
+        /// <summary>
+        /// <para>
+        /// A value indicating how more frequently this instance of 
+        /// <see cref="IWeighted"/> should be selected over other instances.
+        /// </para>
+        /// <para>
+        /// For example, if two <see cref="IWeighted">IWeighteds</see> 
+        /// x and y have the weights 1 and 4 respectively then
+        /// y should be selected four times as likely as x.
+        /// </para>
+        /// </summary>
+        int Weight { get; set; }
+    }
+}
---
+++ Archigen/Archigen/WeightedSelector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Archigen
+{
+    /// <summary>
+    /// A simple generator that returns <see cref="IWeighted"/> elements
+    /// from a specified list. While elements are chosen at random,
+    /// those with higher weights are more likely to be selected.
+    /// </summary>
+    public class WeightedSelector<T> : IGenerator<T> where T : IWeighted
+    {
+        protected Random _random;
+
+        /// <summary>
+        /// The list of <see cref="IWeighted"/> elements that the generator will
+        /// use to generate output during calls to <see cref="Next()"/>.
+        /// Values with higher weights are more likely to be selected.
+        /// </summary>
+        public List<T> Values { get; set; }
+
+        /// <summary>
+        /// Creates a new <see cref="WeightedSelector{T}"/>
+        /// that will return random <see cref="IWeighted"/> elements
+        /// from the specified list. Values with higher weights are more
+        /// likely to be selected.
+        /// </summary>
+        public WeightedSelector(IEnumerable<T> values)
+        {
+            _random = new Random();
+            Values = new List<T>(values);
+        }
+
+        /// <summary>
+        /// Returns a random element from a list of 
+        /// <see cref="IWeighted"/> values.
+        /// </summary>
+        public virtual T Next()
+        {
+            int totalWeight = Values.Sum(x => x.Weight);
+            int randomSelection = _random.Next(totalWeight);
+
+            int runningTotal = 0;
+
+            for (int i = 0; i < Values.Count; i++)
+            {
+                runningTotal += Values[i].Weight;
+
+                if (randomSelection < runningTotal)
+                {
+                    return Values[i];
+                }
+            }
+
+            throw new InvalidOperationException("A random choice could not be made. The list may be empty or there are elements with non-positive weights.");
+
+        }
+    }
+}
